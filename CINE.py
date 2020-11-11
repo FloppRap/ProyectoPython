76f783485f0a22306ec3695ca11a4ccd2db8df7e
FloppRap <gabyingds01@gmail.com>
2020-11-11 18:20:48 -0300

    Primer version de CINE

---
+++ CINE.py
@@ -0,0 +1,10 @@
+class Cine:
+    cartelera = {
+        "Mickey":[0,100],
+        "Ted":[18,100]
+    }
+    cantidadButacas = 100
+    cantidadClientes = 45
+
+    def __init__(self):
+        pass
